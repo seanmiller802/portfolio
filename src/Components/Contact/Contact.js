918ddcb0b0acaa3f83cbcb2fde13c6c58d6342fe
sean miller <>
2019-03-18 12:50:20 -0700

    No more bacon rain

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react';
-
 import './Contact.css';
 import ContactSidebar from './ContactSidebar/ContactSidebar';
 
@@ -9,7 +8,6 @@ class Contact extends Component {
 
     this.state = {
       showContactSidebar: false,
-      bacon: [],
     }
   }
 
@@ -18,73 +16,49 @@ class Contact extends Component {
       showContactSidebar: !this.state.showContactSidebar
     })
   }
-
-  rainBacon = async () => {
-
-    const { bacon } = this.state;
-
-    const random = Math.floor(Math.random() * 9) + 1;
-
-    this.setState({
-      bacon: [...bacon, random]
-    })
-    
-  }
   
   render() {
-    const { showContactSidebar, bacon } = this.state;
-
-    const baconRain = bacon.map((i) => {
-      return (
-        <i key={i} className={`fas fa-bacon rain${i}`}></i>
-      )
-    });
+    const { showContactSidebar } = this.state;
 
     return (
       <Fragment>
-      <div className="contact">
-      {baconRain}
-        <div className="contact-content">
-          <h2>How you can <span id="skills-highlight">get in touch</span></h2>
-          <div id="contact-main">
-            <ul className="contact-list">
-              <li>
-                <i className="fas fa-envelope contact-list-icon"></i> 
-                <a onClick={this.handleSidebar}>
-                  <span className="contact-list-text">miller.stowe@gmail.com</span>
-                </a>
-              </li>
-              <li>
-                <i className="fab fa-github contact-list-icon"></i> 
-                <a href="https://github.com/seanmiller802" target="_blank">
-                  <span className="contact-list-text">seanmiller802</span>
-                </a>
-              </li>
-              <li>
-                <i className="fab fa-medium contact-list-icon"></i> 
-                <a href="https://medium.com/@miller.stowe" target="_blank">
-                  <span className="contact-list-text">@miller.stowe</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="side-tabs">
+        <div className="contact">
+          <div className="contact-content">
+            <h2>How you can <span id="skills-highlight">get in touch</span></h2>
+            <div id="contact-main">
+              <ul className="contact-list">
+                <li>
+                  <i className="fas fa-envelope contact-list-icon"></i> 
+                  <a onClick={this.handleSidebar}>
+                    <span className="contact-list-text">miller.stowe@gmail.com</span>
+                  </a>
+                </li>
+                <li>
+                  <i className="fab fa-github contact-list-icon"></i> 
+                  <a href="https://github.com/seanmiller802" target="_blank">
+                    <span className="contact-list-text">seanmiller802</span>
+                  </a>
+                </li>
+                <li>
+                  <i className="fab fa-medium contact-list-icon"></i> 
+                  <a href="https://medium.com/@miller.stowe" target="_blank">
+                    <span className="contact-list-text">@miller.stowe</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="side-tabs">
               <div className="side-tab-1" onClick={this.handleSidebar}>
                 <i className="fas fa-comment"></i>
                 <span>Say Hello</span>
               </div>
-              <div className="side-tab-2" onClick={this.rainBacon}>
-                <i className="fas fa-bacon"></i>
-                <span id="rain-bacon">Rain Bacon</span>
-              </div>
             </div>
             <ContactSidebar handleSidebar={this.handleSidebar} visibility={showContactSidebar ? 'visible' : 'hidden'} />
+          </div>
         </div>
-      </div>
       </Fragment>
-    )
+    );
   }
-  
 }
 
 export default Contact;
